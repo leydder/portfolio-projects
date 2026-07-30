51d20cf7c000126f39ab62a3d54d867f91603514
Leydder Bermudez <leyderalexander@gmail.com>
2026-07-30 11:35:23 -0500

    Setup inicial: proyecto Spring Boot con Hello World, H2 persistente y consola habilitada

---
+++ constructora-app/src/test/java/com/legp/constructora/ConstructoraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legp.constructora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructoraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
